6cbdf78c4d216ce1e43447513f5cf283e1776f89
Joe Begley <joebegley15@gmail.com>
2018-09-22 13:09:26 -0400

    Edge case fix implemented

--- english_beggars/main.js
+++ english_beggars/main.js
@@ -2,6 +2,7 @@ def beggars(values, n):
     arr = []
     for i in range(0,n):
         arr.append(0)
-    for i in range(0,len(values)):
-        arr[i%n] += values[i]
+    if n != 0:
+        for i in range(0,len(values)):
+            arr[i%n] += values[i]
     return arr
